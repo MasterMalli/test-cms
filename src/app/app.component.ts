33132af24b5392a16bef6962cc68049a41c13936
Malachi Jones <mastermalli0@gmail.com>
2018-06-01 12:26:50 +0100

    Updated styling & improved responsive design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'managment';
+  title = 'Management';
   links: any[];
 
   constructor() {
